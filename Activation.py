5cf485935115120ea66145c0237820b844ee7292
kenp-io <account@kenp.io>
2020-05-21 15:26:19 +0200

    sigmoid activation

--- Activation.py
+++ Activation.py
@@ -29,3 +29,15 @@ class Tanh(object):
         
         return torch.mul(gradwrtoutput, 1./(torch.cosh(self.s)**2))
     
+class Sigmoid(object):
+
+    def forward(self, x):
+    
+        self.s = x
+        return 1. / self.s.exp().add(1)
+    
+
+    def backward(self, gradwrtoutput):
+        
+        return torch.mul(gradwrtoutput, torch.mul(self.s,self.s.mul(-1).add(1)))
+    
