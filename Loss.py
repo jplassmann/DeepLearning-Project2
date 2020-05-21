3a51518d4da8fd37f81e7be54cf78c02b87bb59d
kenp-io <account@kenp.io>
2020-05-21 16:08:23 +0200

    binary cross entropy loss

--- Loss.py
+++ Loss.py
@@ -10,3 +10,17 @@ class LossMSE(object):
     def backward(self, x, y):
         
         return 2 * (x - y)
+
+class LossBCE(object):
+
+
+    def forward(self, x, y):
+        
+        return - y * x.log() - ( 1 - y ) * torch.log(1-x)
+        
+    
+
+    def backward(self, x, y):
+        
+        return - y / x + ( 1 - y ) / (1 - x )
+
