418cf1eb00dfae65b9d39f6e57d1c91336c5acaf
alaurens <laurens_antoine@hotmail.fr>
2020-05-21 18:54:49 +0200

    modified main

--- optimiser.py
+++ optimiser.py
@@ -3,19 +3,7 @@ import parameter
 
 class SGD(object):
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     def __init__(self, parameters, lr=0.005):
-=======
-    def __init__(self, parameters, lr=0.01):
->>>>>>> Changerd names and added optimiser and parameters
-=======
-    def __init__(self, parameters, lr=0.005):
->>>>>>> Optimisatino debug
-=======
-    def __init__(self, parameters, lr=0.005):
->>>>>>> b5a787b4618340bb03d5ef9efd504ff08192613f
         self.parameters = parameters
         self.lr = lr
 
@@ -24,32 +12,6 @@ class SGD(object):
         for param in self.parameters:
             param.zero_grad()
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     def step(self):
         for param in self.parameters:
             param.value -= self.lr * param.grad
-<<<<<<< HEAD
-=======
-
-    def step():
-=======
-    def step(self):
->>>>>>> Optimiser works
-        for param in self.parameters:
-            param.value -= self.lr * param.grad
-
-p = parameter.Parameter((1,2,3))
-
-s = SGD(p)
-
-p.zero_grad()
->>>>>>> Changerd names and added optimiser and parameters
-=======
->>>>>>> Optimisatino debug
-=======
-    def step(self):
-        for param in self.parameters:
-            param.value -= self.lr * param.grad
->>>>>>> b5a787b4618340bb03d5ef9efd504ff08192613f
--- parameter.py
+++ parameter.py
@@ -3,11 +3,6 @@ import torch
 class Parameter(object):
 
     def __init__(self, size):
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b5a787b4618340bb03d5ef9efd504ff08192613f
         self.value = torch.zeros(size)
         self.grad = torch.zeros(size)
 
@@ -19,25 +14,3 @@ class Parameter(object):
 
     def add_grad(self, grad):
         self.grad += grad
-<<<<<<< HEAD
-=======
-        self.parameter = torch.zeros(size)
-=======
-        self.value = torch.zeros(size)
->>>>>>> Optimiser works
-        self.grad = torch.zeros(size)
-
-    def set_value(self, value):
-        self.value = value
-
-    def zero_grad(self):
-        self.grad.zero_()
-<<<<<<< HEAD
->>>>>>> Changerd names and added optimiser and parameters
-=======
-
-    def add_grad(self, grad):
-        self.grad += grad
->>>>>>> Optimiser works
-=======
->>>>>>> b5a787b4618340bb03d5ef9efd504ff08192613f
