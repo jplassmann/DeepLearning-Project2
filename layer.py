376263056c703d1359b370125318313b21b004b6
alaurens <laurens_antoine@hotmail.fr>
2020-05-21 16:30:56 +0200

    Optimiser works

--- layer.py
+++ layer.py
@@ -1,5 +1,7 @@
 import torch
 import nnmodule
+import parameter
+import math
 
 class Linear(nnmodule.NNModule):
 
@@ -11,36 +13,37 @@ class Linear(nnmodule.NNModule):
 
         self.eta = 0.01
         #self.params = torch.normal(0, 1, (self.nb_output, self.nb_input))
-        self.params = torch.empty(self.nb_output, self.nb_input).uniform_(-1/math.sqrt(self.nb_input), 1/math.sqrt(self.nb_input))
+        self.params = parameter.Parameter((nb_output, nb_input))
+        self.params.set_value(torch.empty(nb_output, nb_input).uniform_(
+            -1/math.sqrt(self.nb_input), 1/math.sqrt(self.nb_input)))
+        # self.params = torch.empty(self.nb_output, self.nb_input).uniform_(
+        #     -1/math.sqrt(self.nb_input), 1/math.sqrt(self.nb_input))
 
         #self.b = torch.normal(0, 1, (1, self.nb_output))
-        self.b = torch.empty(1, self.nb_output).uniform_(-1/math.sqrt(self.nb_input), 1/math.sqrt(self.nb_input))
+        # self.b = torch.empty(1, self.nb_output).uniform_(
+        #     -1 / math.sqrt(self.nb_input), 1/math.sqrt(self.nb_input))
+        self.b = parameter.Parameter((1, nb_output))
+        self.b.set_value(torch.empty(1, nb_output).uniform_(
+            -1 / math.sqrt(self.nb_input), 1/math.sqrt(self.nb_input)))
 
-
-    def forward ( self , * input_ ) :
-
+    def forward(self, *input_ ) :
 
         self.input = input_[0]
-        self.s = torch.mm(self.params,self.input.t()).t() + self.b
+        self.s = torch.mm(self.params.value, self.input.t()).t() + self.b.value
         return self.s
 
-
-
-
-    def backward ( self , * gradwrts ) :
+    def backward(self, *gradwrts) :
 
         gradwrtsTensor = gradwrts[0]
 
-        gradwrparams = torch.mm(gradwrtsTensor.t() , self.input)/len(self.input)
+        gradwrparams = torch.mm(gradwrtsTensor.t() , self.input) / len(self.input)
 
-        self.params -= self.eta*gradwrparams
-        self.b -= self.eta * gradwrtsTensor.sum(0)/len(self.input)
+        self.params.add_grad(gradwrparams)
+        self.b.add_grad(gradwrtsTensor.sum(0) / len(self.input))
 
-        gradwrtxl = torch.mm(gradwrtsTensor, self.params)
+        gradwrtxl = torch.mm(gradwrtsTensor, self.params.value)
 
         return gradwrtxl
-
-
 
     def parameters(self):
         return [self.params, self.b]
