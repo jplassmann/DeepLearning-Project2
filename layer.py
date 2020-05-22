7ad52cf64e09fb1db3af0c363c1cb47ad3ef194d
Jérémy Plassmann <jeremy.plassmann@gmail.com>
2020-05-22 17:20:35 +0200

    layer tout propre

--- layer.py
+++ layer.py
@@ -20,16 +20,10 @@ class Linear(nnmodule.NNModule):
         self.nb_output = nb_output
 
         self.eta = 0.01
-        #self.params = torch.normal(0, 1, (self.nb_output, self.nb_input))
+        
         self.params = parameter.Parameter((nb_output, nb_input))
         self.params.set_value(torch.empty(nb_output, nb_input).uniform_(
             -1/math.sqrt(self.nb_input), 1/math.sqrt(self.nb_input)))
-        # self.params = torch.empty(self.nb_output, self.nb_input).uniform_(
-        #     -1/math.sqrt(self.nb_input), 1/math.sqrt(self.nb_input))
-
-        #self.b = torch.normal(0, 1, (1, self.nb_output))
-        # self.b = torch.empty(1, self.nb_output).uniform_(
-        #     -1 / math.sqrt(self.nb_input), 1/math.sqrt(self.nb_input))
         self.b = parameter.Parameter((1, nb_output))
         self.b.set_value(torch.empty(1, nb_output).uniform_(
             -1/math.sqrt(self.nb_input), 1/math.sqrt(self.nb_input)))
