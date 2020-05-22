6e0971e00c0bdbd742fccee82f06feb77f231e30
Jérémy Plassmann <jeremy.plassmann@gmail.com>
2020-05-22 16:19:53 +0200

    comments layer.py

--- layer.py
+++ layer.py
@@ -1,13 +1,21 @@
+""" The different kind of layers are defined here. Only the Linear layer is defined at the moment."""
+
 import torch
 import nnmodule
 import parameter
 import math
 
 class Linear(nnmodule.NNModule):
-
+    """ Class defining the linear layer"""
 
     def __init__(self, nb_input, nb_output):
-
+        """ Initialize a linear layer 
+        
+            Args:
+                nb_input:  length of the input
+                nb_output: length of the output
+        """
+        
         self.nb_input = nb_input
         self.nb_output = nb_output
 
@@ -27,13 +35,28 @@ class Linear(nnmodule.NNModule):
             -1/math.sqrt(self.nb_input), 1/math.sqrt(self.nb_input)))
 
     def forward(self, *input_) :
-
+        """ Compute the forward pass of the linear layer 
+        
+            Args: 
+                input_: tuple with the input tensor in the first position
+                
+            Returns: output of the layer
+        """
+        
         self.input = input_[0]
         self.s = torch.mm(self.params.value, self.input.t()).t() + self.b.value
         return self.s
 
     def backward(self, *gradwrts) :
-
+        """ Compute the backward pass of the linear layer. In this function,
+            the gradient with respect to the weights are added to the Parameter.
+        
+            Args:
+                gradwrts: tuple with the gradient with respect to s in first position
+                
+            Returns: gradient with respect to the output of the layer
+        """
+        
         gradwrtsTensor = gradwrts[0]
 
         gradwrparams = torch.mm(gradwrtsTensor.t() , self.input) / len(self.input)
@@ -46,4 +69,6 @@ class Linear(nnmodule.NNModule):
         return gradwrtxl
 
     def parameters(self):
+        """ Returns: weights and biases of the layer."""
+        
         return [self.params, self.b]
