fe9337db478b05bf1b7c9ccec290cbfdb1f7e11f
alaurens <laurens_antoine@hotmail.fr>
2020-05-22 07:48:48 +0200

    Reuploaded Binaray cross entropy loss

--- loss.py
+++ loss.py
@@ -5,3 +5,12 @@ class LossMSE(object):
 
     def backward(self, x, y):
         return 2 * (x - y)
+
+
+class LossBCE(object):
+
+    def forward(self, x, y):
+        return - y * x.log() - ( 1 - y ) * torch.log(1-x)
+
+    def backward(self, x, y):
+        return - y / x + ( 1 - y ) / (1 - x )
